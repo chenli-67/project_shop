9b7dacb99faa5ad4e3c681b0ab9a689c33ad4fbf
李家锐 <516379822@qq.com>
2020-11-15 18:51:19 +0800

    完成了登录功能

---
+++ src/plugins/element.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+
+
+import {
+    Button,
+    Form,
+    FormItem,
+    input,
+    Message
+} from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(input)
+Vue.prototype.$message = Message
