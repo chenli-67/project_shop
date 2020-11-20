6b24f1b28bd53a6e103fe05dac5c61795dce5aa9
李家锐 <516379822@qq.com>
2020-11-20 16:17:56 +0800

    完成用户列表的开发

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import User from '../components/user/User.vue'
 
 
 
@@ -10,7 +12,15 @@ Vue.use(VueRouter)
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home }
+  {
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: User }
+    ]
+  }
 ]
 
 const router = new VueRouter({
@@ -24,10 +34,10 @@ router.beforeEach((to, from, next) => {
   //next 是一个函数 表示放行
   //    next() 放行        next('/login') 强制跳转
 
-  if(to.path === '/login') return next()
+  if (to.path === '/login') return next()
   // 获取token 
   const tokenStr = window.sessionStorage.getItem('token')
-  if(!tokenStr) return next('/login')
+  if (!tokenStr) return next('/login')
   next()
 })
 
